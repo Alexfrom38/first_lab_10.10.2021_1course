35345521f8b2efce8fd725d3105635ceab2f87eb
sasha.lyceum38@outlook.com <sasha.lyceum38@outlook.com>
2021-10-22 00:40:28 +0300

    done

--- first_lab_1course/Source.c
+++ first_lab_1course/Source.c
@@ -1,25 +1,70 @@
 #include<stdio.h>
-#include <time.h>
-#include <conio.h>
+#include<stdlib.h>
+
+int remains(double* double_number)
+{
+	int rem = 0;	
+	rem = (*double_number - (int)*double_number) * 1000000;
+	rem /= 100;
+	return rem;
+}
 
 int main(void)
-{/// пока что не закончено
-	srand(time(NULL));
-	int min_of_numb = 0, max_of_numb = 0, count_of_numb = 0;
-	double r1;
+{
+	char fl = 0;
+	int min_numb = 0, max_numb = 0, count_of_numb = 0;
+		double current_random_numb = 0, sum = 0;
+	double* array_of_numbers; 
+	int* array_of_remains;
 
 	printf("input numbers: min,max, and count \n");
-	scanf_s("%d", &min_of_numb);
-	scanf_s("%d", &max_of_numb);
+	
+	printf("input min of number: ");
+	scanf_s("%d", &min_numb);
+
+	printf("input max of number: ");
+	scanf_s("%d", &max_numb);
+
+	printf("input count of number: ");
 	scanf_s("%d", &count_of_numb);
 
-
-
-	for (int i = 0; i <= count_of_numb; i++)
+	array_of_numbers = (double*)malloc(count_of_numb * sizeof(double));
+	array_of_remains = (int*)malloc(count_of_numb * sizeof(int));
+	
+	if (array_of_numbers == 0)
 	{
-		r1 = (double) ( rand()%(int)(10*((max_of_numb - min_of_numb+1)+1))) /10.0;
-
-		printf("%lf \n ", r1);
+		printf("Error: memory is not allocated \n");
+		return 0;
 	}
+
+	if (max_numb < min_numb)
+	{
+		printf("Error: min number > max number");
+		return 0;
+	}
+	
+	if (count_of_numb == 0)
+	{
+		printf("Error: count of number = 0");
+		return 0;
+	}
+	
+	
+	for (int i = 0; i < count_of_numb; i++)
+	{
+		fl = 0;
+		current_random_numb = (((double)rand()) / RAND_MAX) * ((double)max_numb - (double)min_numb) + min_numb;
+		array_of_remains[i] = remains(&current_random_numb);
+		
+		sum += current_random_numb;
+		for (int q = 0; q < i; q++)
+		{
+			if (i == array_of_remains[q])
+				fl = 1;
+		}
+	
+	}
+	printf(" sum  = %lf ", sum);
+	
 	return 0;
 }
