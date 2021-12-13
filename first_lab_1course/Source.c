5d9e070b002925d91a5b550d5cd0fa6cdbb9399a
Alexxxx <56801896+Alexfrom38@users.noreply.github.com>
2021-12-14 00:24:37 +0300

    Update Source.c

--- first_lab_1course/Source.c
+++ first_lab_1course/Source.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int remains(double* double_number)
+int remains(double double_number)
 {
 	int rem = 0;	
-	rem = (*double_number - (int)*double_number) * 1000000;
-	rem /= 100;
+	rem = (	double_number - (int)double_number  ) * 1000;
+	if (rem == 0)
+		return 0;
+	else
+	while (rem % 10 == 0)
+		rem = rem / 10;
+
 	return rem;
 }
 
@@ -17,6 +22,7 @@ int main(void)
 	double current_random_numb = 0, sum = 0 ;
 
 	int* array_of_remains;
+	double* array_of_numbers;
 
 	printf("input numbers: min,max, and count \n");
 	
@@ -30,13 +36,18 @@ int main(void)
 	scanf_s("%d", &count_of_numb);
 
 	array_of_remains = (int*)malloc(count_of_numb * sizeof(int));
+	array_of_numbers = (double*)malloc(count_of_numb * sizeof(double));
 	
 	if (array_of_remains == 0)
 	{
 		printf("Error: memory is not allocated \n");
 		return 0;
 	}
-
+	if (array_of_numbers == 0)
+	{
+		printf("Error: memory is not allocated \n");
+		return 0;
+	}
 	if (max_numb < min_numb)
 	{
 		printf("Error: min number > max number");
@@ -52,26 +63,29 @@ int main(void)
 	
 	for (int i = 0; i < count_of_numb; i++)
 	{
-		fl = 0;
-		current_random_numb = (((double)rand()) / RAND_MAX) * ((double)max_numb - (double)min_numb) + min_numb;
-		array_of_remains[i] = remains(&current_random_numb);
-	
-		sum += current_random_numb;
-		for (int q = 0; q <= i; q++)
-		{
-			if (i == array_of_remains[q])
-				fl = 1;
-		}
 		
-		if (fl == 1)
-		{
-			sum -= current_random_numb;
-		}
+		current_random_numb = (((double)rand()) / RAND_MAX) * ((double)max_numb - (double)min_numb) + min_numb;
+		array_of_numbers[i] = current_random_numb;
 
+		printf("%lf \n", current_random_numb);
+
+		array_of_remains[i] = remains(current_random_numb);
+		printf("%d \n", array_of_remains[i]);
 	}
 	
+
+	for (int i = 0; i < count_of_numb; i++)
+		if (array_of_remains[i] < count_of_numb)
+		{
+			sum = sum - array_of_numbers[array_of_remains[i]];
+			array_of_numbers[array_of_remains[i]] = 0.0;
+		}
+
+	for (int i = 0; i < count_of_numb; i++)
+		sum = sum + array_of_numbers[i];
 	printf("sum  = %lf \n", sum);
 
 	free(array_of_remains);
+	free(array_of_numbers);
 	return 0;
 }
